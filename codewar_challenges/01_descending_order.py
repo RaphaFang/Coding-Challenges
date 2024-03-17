65cc094dc1a5baa020f2540ee21d80d8abf530b2
RaphaFang <fangsihyu0211@gmail.com>
2024-03-17 23:08:32 +0800

    codewar first

---
+++ codewar_challenges/01_descending_order.py
@@ -0,0 +1,15 @@
+num = 51
+
+def descending_order(num):
+    num_list = [int(i) for i in str(num)]
+    aaa = ''
+    for n in sorted(num_list, reverse=True):
+        aaa += str(n)
+    return int(aaa)
+
+descending_order(num)
+
+# ___________________________________________________
+# even better solution
+def descending_order(num):
+    return int("".join(sorted([num for num in str(num)], reverse=True)))
