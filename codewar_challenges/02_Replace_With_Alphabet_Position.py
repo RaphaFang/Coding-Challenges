af1858182aff3b87a8da5b4f7f1378277bd7f786
RaphaFang <fangsihyu0211@gmail.com>
2024-03-20 16:28:07 +0800

    second

--- codewar_challenges/02_Replace_With_Alphabet_Position.py
+++ codewar_challenges/02_Replace_With_Alphabet_Position.py
@@ -48,3 +48,19 @@ def alphabet_position(text):
 # 玄找是否在字母列表裡面
 # 是，尋找該自在字母列表的順位
 # 否，pass
+            
+
+#  _________________________________________
+def alphabet_position(text):
+    alph_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alph_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    text = text.upper()   # THE SUNSET SETS AT TWELVE O' CLOCK.
+    text_list = list(text)  # ['T', 'H', 'E', ' ', 'S', 'U', 'N', 'S', 'E', 'T', ' ', 'S', 'E', 'T', 'S', ' ', 'A', 'T', ' ', 'T', 'W', 'E', 'L', 'V', 'E', ' ', 'O', "'", ' ', 'C', 'L', 'O', 'C', 'K', '.']
+    output_num = []
+    
+    for l in text_list:
+        if l in alph_list:
+            x = alph_str.find(l) + 1
+            output_num.append(str(x))
+    final = ' '
+    print(f'"{final.join(output_num)}"')
