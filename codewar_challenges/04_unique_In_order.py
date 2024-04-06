f7a7c3ae4ca13c3bea999bd9be032c0d64e71715
RaphaFang <fangsihyu0211@gmail.com>
2024-04-06 17:39:02 +0800

    first

--- codewar_challenges/04_unique_In_order.py
+++ codewar_challenges/04_unique_In_order.py
@@ -1,19 +1,23 @@
 tur = (1,2,3)
 str = "ABCDE"
-list1 = [1, 2, 2, 3, 3, 4, 5]
+list1 = [1, 2, 2, 3, 3, 4, 5, 5]
 
 
 def unique_in_order(sequence):
-    for n in list(sequence):
+    newlist = []
+    for n in range(len(list(sequence))):
         print(n)
-        newlist = []
-        if n == n+1:
-            print(n + "repeat")
-            pass
+        try:
+            if list(sequence)[n] == list(sequence)[n+1]:
+                pass
+        except IndexError:
+            if list(sequence)[n] == list(sequence)[n-1]:
+                pass
+            else:
+                newlist.append(list(sequence)[n])
         else:
-            newlist.append(n)
-    print(newlist)
-
-# print(list(str))
-            
-unique_in_order(str)
+            print(list(sequence)[n])
+            newlist.append(list(sequence)[n])
+    print(newlist)           
+     
+unique_in_order(list1)
