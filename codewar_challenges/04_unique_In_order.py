8c5256c7c20982bea65d397314970f37c7a981c4
RaphaFang <fangsihyu0211@gmail.com>
2024-04-12 18:19:43 +0800

    first challenge

--- codewar_challenges/04_unique_In_order.py
+++ codewar_challenges/04_unique_In_order.py
@@ -1,24 +1,35 @@
-tur = (1,2,3,4,5,5)
-str = "AAABCDE"
-list1 = [1, 2, 2, 3, 3, 4, 5, 5]
-
-
 def unique_in_order(sequence):
-    newlist = []
-    for n in range(len(list(sequence))):
-        print(n)
-        try:
-            if list(sequence)[n] == list(sequence)[n+1]:
+    if bool(sequence)!=False:
+        ori=list(sequence)
+        without=list(sequence)[1:]
+        output=[]
+        output.append(ori[0])
+        for n in range(len(without)):
+            if without[n]==ori[n]:
                 pass
             else:
-                print(list(sequence)[n])
-                newlist.append(list(sequence)[n])
-        except IndexError:
-            if list(sequence)[n] == list(sequence)[n-1]:
-                pass
-            else:
-                newlist.append(list(sequence)[n])
-        
-    return newlist
+                output.append(without[n])
+        return output
+    else:
+        return []
 
-unique_in_order(str)
+
+unique_in_order('AAAABBBCCDAABBB') #== ['A', 'B', 'C', 'D', 'A', 'B']
+unique_in_order('ABBCcAD')         #== ['A', 'B', 'C', 'c', 'A', 'D']
+unique_in_order([1, 2, 2, 3, 3])   #== [1, 2, 3]
+unique_in_order((1, 2, 2, 3, 3))   #== [1, 2, 3]
+unique_in_order([]) #== []
+
+# py切片語法
+# https://zhuanlan.zhihu.com/p/79541418
+
+# ------------------------------------------------------------------
+from itertools import groupby
+
+def unique_in_order(iterable):
+    return [k for (k, _) in groupby(iterable)]
+# ------------------------------------------------------------------
+unique_in_order = lambda l: [z for i, z in enumerate(l) if i == 0 or l[i - 1] != z]
+# ------------------------------------------------------------------
+def unique_in_order(iterable):
+    return [ ch for i, ch in enumerate(iterable) if i == 0 or ch != iterable[i - 1] ]
