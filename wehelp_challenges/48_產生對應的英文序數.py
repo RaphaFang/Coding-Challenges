ccb22da33a8fa99bc02871d5a5b320ea5a294447
RaphaFang <fangsihyu0211@gmail.com>
2024-04-15 10:39:01 +0800

    1st

--- wehelp_challenges/48_產生對應的英文序數.py
+++ wehelp_challenges/48_產生對應的英文序數.py
@@ -1,23 +1,24 @@
 def generateOrdinalNumber(number):
     lis_num = list(str(number))
-    try:
-        if int(lis_num[-2])==0:
-            if int(lis_num[-1])==1:
-                return( str(number)+"st")
-            if int(lis_num[-1])==2 :
-                return( str(number)+"nd")
-            if int(lis_num[-1])==3:
-                return( str(number)+"rd")
-    except IndexError:
+    if len(lis_num)>=2:
+        last2 = int(lis_num[-2] +lis_num[-1])
+        if int(lis_num[-1])==1 and last2!=11:
+            return( str(number)+"st")
+        elif int(lis_num[-1])==2 and last2!=12:
+            return( str(number)+"nd")
+        elif int(lis_num[-1])==3 and last2!=13:
+            return( str(number)+"rd")
+        else:
+            return( str(number)+"th")
+    else:
         if int(lis_num[-1])==1:
             return( str(number)+"st")
-        if int(lis_num[-1])==2 :
+        elif int(lis_num[-1])==2 :
             return( str(number)+"nd")
-        if int(lis_num[-1])==3:
+        elif int(lis_num[-1])==3:
             return( str(number)+"rd")
-    else:
-        return( str(number)+"th")
-
+        else:
+            return( str(number)+"th")
         
     
 
