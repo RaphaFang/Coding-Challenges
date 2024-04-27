dd4a78d4f4f3059e904c5e89148d15388cb07765
RaphaFang <fangsihyu0211@gmail.com>
2024-04-27 16:20:31 +0800

    donee

--- codewar_challenges/6kyu_Build a pile of Cubes.py
+++ codewar_challenges/6kyu_Build a pile of Cubes.py
@@ -2,23 +2,23 @@ def find_nb(m):
     total = 0
     trying_num = 0
 
-    while total<m:
-        total = 0
-        trying_num+=1
-        for num in range(trying_num):
-            total += (num+1)**3
-            
+    while total < m:
+        trying_num += 1
+        total += trying_num**3
+
+#   這個方式確實相較之下需要運算更多次，每次while都要跑一個for loop
+#     while total < m:
+#         total = 0
+#         trying_num+=1
+#         for num in range(trying_num):
+#             total += (num+1)**3
+
     if total > m:
         print (-1)
     if total == m:
         print (trying_num)
-
-
-# 𝑛3+(𝑛−1)3+(𝑛−2)3+...+13= 𝑚
     
 # findNb(1071225) --> 45
 # findNb(91716553919377) --> -1
         
-
-        
-find_nb(26825883955641)
+find_nb(9796076958746727442)
