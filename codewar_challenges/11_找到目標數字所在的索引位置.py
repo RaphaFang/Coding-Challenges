35e742541ae12d1aff683422be2c0c89aa94e6a4
RaphaFang <fangsihyu0211@gmail.com>
2024-04-29 09:33:21 +0800

    done

--- codewar_challenges/11_找到目標數字所在的索引位置.py
+++ codewar_challenges/11_找到目標數字所在的索引位置.py
@@ -1,11 +1,11 @@
 def findIndex(nums, target):
     for n in nums:
         if n == target:
-            print( nums.index(n))
-        else:
-            print(-1)
+            return( nums.index(n))
+    if target not in nums:
+        return(-1)
 
-findIndex([3,2,1,5,10], 1)
+findIndex([3,2,1,5,10], 7)
 
 list = [1,2,3,4,5,6,7,8,9,10]
 
