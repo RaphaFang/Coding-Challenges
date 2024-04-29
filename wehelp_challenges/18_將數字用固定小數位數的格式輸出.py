a35ad0feac98d833d826b3273114cd230d2243b9
RaphaFang <fangsihyu0211@gmail.com>
2024-04-29 10:09:08 +0800

    done

---
+++ wehelp_challenges/18_將數字用固定小數位數的格式輸出.py
@@ -0,0 +1,13 @@
+# def formatFloat(n):
+#     return str(round(n, 2))
+
+def formatFloat(n): 
+    # print(type(n))
+    print("%.2f" % n)
+    # print(type("%.2f" % n))
+
+
+formatFloat(99.4454)
+
+# 完全搞不懂這啥
+# https://blog.csdn.net/Jerry_1126/article/details/85009810
