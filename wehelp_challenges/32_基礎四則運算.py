a82ca7168aa1d62ce17e4899771f5ff0258e01b3
RaphaFang <fangsihyu0211@gmail.com>
2024-04-29 18:07:59 +0800

    done

---
+++ wehelp_challenges/32_基礎四則運算.py
@@ -0,0 +1,9 @@
+def calculate(n1, n2, op):
+    if op == "+":
+        return n1+n2
+    if op == "-":
+        return n1-n2
+    if op == "*":
+        return n1*n2
+    if op == "/":
+        return n1/n2
---
+++ wehelp_challenges/33_計算 X 的 Y 次方.py
@@ -0,0 +1,5 @@
+def power(x, y):
+    if x>=-30 and x<=30 and y>=0 and y<=10:
+        return  x**y
+    else:
+        return None
