6ba149611b15679f6cc1e0a3dfadbef49c481915
RaphaFang <fangsihyu0211@gmail.com>
2024-05-01 16:35:06 +0800

    done by the help from gpt

--- wehelp_challenges/21_最大連續 0 的數量.py
+++ wehelp_challenges/21_最大連續 0 的數量.py
@@ -1,25 +1,42 @@
 def findMaxZero(nums):
-    con = 0
-    for n in range(len(nums)-1):
-        # print(nums[n])
-        if nums[n] == nums[n+1] and nums[n] ==0:
-            con +=1
+    max_consecutive_zeros = 0
+    current_zeros = 0
 
-    if con !=0:
-        con+=1
+    for num in nums:
+        if num == 0:
+            current_zeros += 1
+            max_consecutive_zeros = max(max_consecutive_zeros, current_zeros)
+            # max 這裡max比較（）中的兩個input或是多個，推出去最大的值
+            # 如果遇到1，導致脫離loop，current_zeros會規零，但是max_consecutive_zeros不會
+            # 仍會透過max()中的 max_consecutive_zeros，確保最大值的紀錄
+        else:
+            current_zeros = 0  # Reset the zero counter when a 1 is encountered
+    return( max_consecutive_zeros)
 
-    if len(nums)==1 and nums[0]==0:
-        con+=1
+# def findMaxZero(nums):
+#     con = 0
+#     li = []
 
-    print (con)
+#     if len(nums)==1 and nums[0]==0:
+#         print (1)
+#     else:
+#         for n in range(len(nums)-1):
+#             if nums[n] == nums[n+1] and nums[n]==0:
+#                 con +=1
+#             else:
+#                 con+=1
+#                 li.append(con)
+#                 con=0
+#         li.append(con)
+#     print(li)
 
 
 
-findMaxZero([0,0,1,0,0])
+findMaxZero([0,0,0,1,0])
 
 
 
 # test = [1,2,3,4,5, 5]
 
-# # print(type(int(test[0::-1])))
+# print(type(int(test[0::-1])))
 # print(max(test))
