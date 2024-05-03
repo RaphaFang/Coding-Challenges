a949a6a7c84986490f8de483304e1aafd0e26658
RaphaFang <fangsihyu0211@gmail.com>
2024-05-03 15:34:26 +0800

    done

--- wehelp_challenges/39_去除重複的資料.py
+++ wehelp_challenges/39_去除重複的資料.py
@@ -1,2 +1,16 @@
 def removeDuplicates(ns):
-    for n in 
+    or_list = []
+    re_list = []
+
+    for n in ns:
+        if ns.count(n) == 1:
+            or_list.append(n)
+        else:
+            if n not in re_list:
+                re_list.append(n)
+                or_list.append(n)
+    return (or_list)
+
+
+    
+removeDuplicates([3, 2, -6, 2, 3, 5, 2])
