5a0b14c2b173ad47c8b3d0b020d95407deb6fabf
RaphaFang <fangsihyu0211@gmail.com>
2024-05-06 13:56:45 +0800

    done

--- codewar_challenges/6kyu_Create Phone Number.py
+++ codewar_challenges/6kyu_Create Phone Number.py
@@ -4,3 +4,19 @@ def create_phone_number(n):
     return( f'({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}')
 
 create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+
+
+def create_phone_number(n):
+	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+
+
+print("网站名：{name}, 地址 {url}".format(name="菜鸟教程", url="www.runoob.com"))
+ 
+# 通过字典设置参数
+site = {"name": "菜鸟教程", "url": "www.runoob.com"}
+print("网站名：{name}, 地址 {url}".format(**site))
+ 
+# 通过列表索引设置参数
+my_list = ['菜鸟教程', 'www.runoob.com']
+print("网站名：{0[0]}, 地址 {0[1]}".format(my_list))  # "0" 是必须的
