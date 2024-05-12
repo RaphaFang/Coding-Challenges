69b3eb527adabb5327ec42a32aff57094f1daca4
RaphaFang <fangsihyu0211@gmail.com>
2024-05-12 21:06:09 +0800

    pp

--- codewar_challenges/4kyu_Next bigger number with the same digits.py
+++ codewar_challenges/4kyu_Next bigger number with the same digits.py
@@ -1,16 +1,18 @@
 from itertools import permutations
-
-
 def next_bigger(n):
     listed_n = [ int(n) for n in list(str(n))]
-    # listed_n.sort(reverse=True)
-    print(listed_n)
+    listed_n.sort(reverse=True)
+    # print(listed_n)
 
-    all_permutations = permutations(listed_n)
-
-    llist = [''.join(str(n)) for n in all_permutations]
-
+    # all_permutations = permutations(listed_n)
+    llist = [int(''.join(str(n)[1:-1].split(', ')))for n in permutations(listed_n)]
+    llist.sort()
     print(llist)
+    for i in llist:
+        if i>n:
+            return(i)
+    return -1
+
 
     # for perm in all_permutations:
     #     # print(''.join(str(perm)))
@@ -22,11 +24,14 @@ def next_bigger(n):
 
 
 
-    # gib = ''
-    # for n in listed_n:
-    #     gib+=str(n)
-    # print(gib)
-    # if n == int(gib):
-    #     return -1
+#     # gib = ''
+#     # for n in listed_n:
+#     #     gib+=str(n)
+#     # print(gib)
+#     # if n == int(gib):
+#     #     print -1
 
-next_bigger(2017)
+next_bigger(12)
+
+# list = [0,1,2,3,4]
+# print(list[1:-1])
