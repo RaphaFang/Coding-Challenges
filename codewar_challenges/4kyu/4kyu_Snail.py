39b5b25af8815618d98f45e8fd3644ebb170fa8d
RaphaFang <fangsihyu0211@gmail.com>
2024-05-18 14:13:57 +0800

    pp

--- codewar_challenges/4kyu/4kyu_Snail.py
+++ codewar_challenges/4kyu/4kyu_Snail.py
@@ -2,29 +2,36 @@ def snail(snail_map):
     if len(snail_map)%2 == 0:
         len_list = [n*2-1 for n in range(len(snail_map)//2 ,0,-1)]
     if len(snail_map)%2 == 1:
-        len_list = [n*2 for n in range(len(snail_map)//2 ,0,-1)]
+        len_list = [n*2 for n in range(len(snail_map)//2 ,0,-1)] +[0]
     print(len_list)
 
-    for n in 
+    len = len(snail_map)
+
+    for index,n in enumerate(len_list):  # [2,0]
+        up_row = snail_map[index][index:n]
+        right_row = [n[-1] for n in snail_map[index:-1]]
+
+        down_row = snail_map[len-1-index][index:n]
+            
 
 
 
 
-    
-    # return snail_map[0]+[snail_map[1][2]]+snail_map[2][::-1]+snail_map[1][:2]
 
-array = [[1,2,3],
-         [8,9,4],
-         [7,6,5]]
 
-print(snail(array))
+#     # return snail_map[0]+[snail_map[1][2]]+snail_map[2][::-1]+snail_map[1][:2]
 
-# print(len(array))
+# array = [[1,2,3],
+#          [8,9,4],
+#          [7,6,5]]
+
+# print(snail(array))
+
+# # print(len(array))
 
 
 # aa = [0,1,2]
-
 # bb = [3,4,5]
-# print(aa[::-1]+bb)  #[0, 1, 2, 3, 4, 5]
+# print(aa[:-1]+bb)  #[0, 1, 2, 3, 4, 5]
 
-# print(bb[:-1])
+# # print(bb[:-1])
