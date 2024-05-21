1ed155506fa216bc1fb8a3d97848bff250c298f7
RaphaFang <fangsihyu0211@gmail.com>
2024-05-21 18:04:29 +0800

    pp

--- codewar_challenges/5kyu/5kyu_A Man and his Umbrellas.py
+++ codewar_challenges/5kyu/5kyu_A Man and his Umbrellas.py
@@ -1,2 +1,14 @@
 def min_umbrellas(weather):
-    pass
+    for index,n in enumerate(weather):
+        if n =="rainy" or n=='thunderstorms':
+            weather[index] = 1
+        else:
+            weather[index] = 0
+    print( weather)
+
+weather = ["overcast", "rainy", "clear", "thunderstorms"]
+print(min_umbrellas(weather))
+
+
+
+the_0_1 = []
